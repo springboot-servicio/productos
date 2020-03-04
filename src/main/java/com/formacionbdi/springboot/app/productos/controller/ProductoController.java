88f7dc02441689285aa3446f3ecaa2e1ee5da835
iscipar <iscipar@hotmail.com>
2020-03-04 16:26:47 +0100

    Se utiliza el servicio commons

--- src/main/java/com/formacionbdi/springboot/app/productos/controller/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controller/ProductoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.formacionbdi.springboot.app.productos.models.entity.Producto;
+import com.formacionbdi.springboot.app.commons.models.entity.Producto;
 import com.formacionbdi.springboot.app.productos.models.service.IProductoService;
 
 @RestController
